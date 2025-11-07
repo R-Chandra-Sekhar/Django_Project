5c253ed277fd0a0d9b54e63db38705361c1e9d8e
R-Chandra-Sekhar <rchandrasekhar334@gmail.com>
2025-11-07 10:42:09 +0530

    initial step

---
+++ myproject/basic/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+# Create your views here.
+def sample(request):
+    return HttpResponse('hello world')
+def sample1(request):
+    return HttpResponse('welcome to django')
+def sampleInfo(request):
+    # data={'name':'sumanth','age':25,'city':'hyd'}
+    data={'result':[4,6,8,9]}
+
+    return JsonResponse(data)
+
+def dynamicResponse(request):
+    name=request.GET.get("name",'')
+    return HttpResponse(f"hello {name}")
